adfd3244a81b01f0de8ef5f20838d096d9005c55
lksin <64049501+lksin@users.noreply.github.com>
2021-07-17 13:35:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-$str1 = "https://pic.lksin.xyz/test1/";
-$str2 = rand(0,29);
+$str1 = "https://pic3.lksin.xyz/pic/main/test/";
+$str2 = rand(0,841);
 $str3 = ".jpg";
-
-$str4 = $str1.$str2.$str3;
+$strn1 = "1 (";
+$strn2 = ")";
+$str4 = $str1.$strn1.$str2.$strn2.$str3;
 echo $str4;
 header("Location: $str4");
 ?>
