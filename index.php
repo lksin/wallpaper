da20cc6ba78561ab29ff8369b0e8f218de1ef21d
lksin <64049501+lksin@users.noreply.github.com>
2021-07-19 18:42:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $sexy=$_GET["id"];
 switch ($sexy) {
     case "all_pic":
@@ -26,7 +27,41 @@ switch ($sexy) {
         $str4 = $str1.$str2.$str3;
         header("Location: $str4");
         break;
+}*/
+<?php
+//存有链接的文件名
+$filename = "RandomPic.txt";
+if(!file_exists($filename)){
+	die('文件不存在');
 }
+
+//从文本获取链接
+$pics = [];
+$fs = fopen($filename, "r");
+while(!feof($fs)){
+	$line=trim(fgets($fs));
+	if($line!=''){
+		array_push($pics, $line);
+	}
+}
+
+//从数组随机获取链接
+$pic = $pics[array_rand($pics)];
+
+//返回指定格式
+$type=$_GET['type'];
+switch($type){
+
+//JSON返回
+case 'json':
+	header('Content-type:text/json');
+	die(json_encode(['pic'=>$pic]));
+
+default:
+	die(header("Location: $pic"));
+}
+
+    
 # $str1 = "https://pic3.lksin.xyz/pic/main/test/";
 # $str2 = rand(0,841);
 # $str3 = ".jpg";
