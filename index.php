97d738ecd8a02992edc6ba63de005e88f9634e1f
lksin <64049501+lksin@users.noreply.github.com>
2021-07-19 18:11:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -19,6 +19,13 @@ switch ($sexy) {
         $str4 = $str1.$strn1.$str2.$strn2.$str3;
         header("Location: $str4");
         break;
+    case "blog_top":
+        $str1 = "https://cdn.jsdelivr.net/gh/PRWYQ/fantasy@1.0/top/";
+        $str2 = rand(1,20);
+        $str3 = ".jpg";
+        $str4 = $str1.$str2.$str3;
+        header("Location: $str4");
+        break;
 }
 # $str1 = "https://pic3.lksin.xyz/pic/main/test/";
 # $str2 = rand(0,841);
