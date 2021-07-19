16d5e53b493f09d9b5048b4d558c641091e7c958
lksin <64049501+lksin@users.noreply.github.com>
2021-07-19 19:20:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ switch ($sexy) {
         $str2 = rand(1,20);
         $str3 = ".jpg";
         $str4 = $str1.$str2.$str3;
+        echo $str4;
         header("Location: $str4");
         break;
 }
